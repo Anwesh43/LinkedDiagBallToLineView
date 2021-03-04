44856bb8eede6ffb1128576faa150b9a6c816229
Anwesh43 <anweshthecool0@gmail.com>
2021-03-05 00:37:39 +0530

    adding DBTLNode class

--- diagballtolineview/src/main/java/com/example/diagballtolineview/DiagBallToLineView.kt
+++ diagballtolineview/src/main/java/com/example/diagballtolineview/DiagBallToLineView.kt
@@ -118,4 +118,45 @@ class DiagBallToLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DBTLNode(var i : Int, val state : State = State()) {
+
+        private var next : DBTLNode? = null
+        private var prev : DBTLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DBTLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDBTLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DBTLNode {
+            var curr : DBTLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
