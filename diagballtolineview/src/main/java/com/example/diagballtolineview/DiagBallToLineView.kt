25edf25ec2c746d4e85e35fd7ef2dd896e9d59a4
Anwesh43 <anweshthecool0@gmail.com>
2021-03-05 00:29:25 +0530

    adding Canvas extension methods

--- diagballtolineview/src/main/java/com/example/diagballtolineview/DiagBallToLineView.kt
+++ diagballtolineview/src/main/java/com/example/diagballtolineview/DiagBallToLineView.kt
@@ -28,3 +28,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawDiagBallToLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val r : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        drawLine(0f, 0f, w * 0.5f * sf1, h * 0.5f * sf1, paint)
+        drawCircle((w / 2 - r) * sf3, (h / 2 - r) * sf3, r * sizeFactor * sf2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawDBTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawDiagBallToLine(scale, w, h, paint)
+}
